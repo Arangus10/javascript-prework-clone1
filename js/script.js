986fef956d706eb504170dae963eddb3379a02bd
Bartek Miastkowski <bartosz1miastkowski@gmail.com>
2022-09-22 11:18:35 +0200

    Add player move 2&3

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 var computerMove;
+var playerMove = "papier";
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let x = x <= 19 && x >= 11;
+let randomNumber;
+randomNumber = Math.floor(Math.random()*x+1)
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
